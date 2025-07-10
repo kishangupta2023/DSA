64ee13d1dd065cd10d70ad80abb0b3b731e560a6
kishan gupta <kishan.2023ug1075@iiitranchi.ac.in>
2025-07-10 17:10:23 +0530

    Add implementations for BFS, DFS, and province counting in graphs

---
+++ Graphs/BFS/basic.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+#include<Queue>
+using namespace std;
+
+class Solution {
+    public: 
+    vector<int> bfsOfGraph(int V,vector<int> adj[]){
+        int vis[V] = {0};
+        vis[0] =1;
+        queue<int> q;
+        q.push(0);
+        vector<int> bfs;
+        while(q.empty()){
+            int node = q.front();
+            q.pop();
+            bfs.push_back(node);
+            for(auto it: adj[node]){
+                if(!vis[it]){
+                    vis[it] = 1;
+                    q.push(it);
+                }
+            }
+        }
+        return bfs;
+    }
+};
+
+//sc -o(3n) & tc - o(n) + o(2E) = (total degree)
+
+int main(){
+
+    return 0;
+}
