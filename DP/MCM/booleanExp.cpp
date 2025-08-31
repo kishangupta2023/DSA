18b479d16b7116360437597ab118ac6e9165f52e
kishan gupta <kishan.2023ug1075@iiitranchi.ac.in>
2025-08-31 13:04:20 +0530

    Refactor evaluateExp function to improve tabulation solution for boolean expression evaluation

--- DP/MCM/booleanExp.cpp
+++ DP/MCM/booleanExp.cpp
@@ -84,7 +84,7 @@ class Solution{
         }
         if(dp[i][j][isTrue] != -1) return dp[i][j][isTrue];
         long long ways = 0;
-        for(int ind =i+1;ind<j-1;j+=2){
+        for(int ind =i+1;ind<j-1;ind+=2){
             long long lT = f(i,ind-1,1,exp,dp);
             long long lF = f(i,ind-1,0,exp,dp);
             long long rT = f(ind+1,j,1,exp,dp);
@@ -126,56 +126,52 @@ class Solution{
 
 
     //Tabulation SOlution 
-    int evaluateExp(string &exp){
-        int n = exp.size();
-        vector<vector<vector<long long>>>dp(n,vector<vector<long long>>(n,vector<long long>(2,0)));
-        for(int i=0;i<n;i++){
-            if(exp[i]=='T'){
-                dp[i][i][1] = 1;
-            }else{
-                dp[i][i][1] = 0;
-            }
-            if(exp[i] == 'F'){
-                dp[i][i][0] = 1;
-            }else{
-                dp[i][i][0]  = 0;
-            }
+   int evaluateExp(string &exp) {
+    int n = exp.size();
+    
+    // 3D DP: dp[i][j][0] -> ways to make substring False
+    //        dp[i][j][1] -> ways to make substring True
+    vector<vector<vector<long long>>> dp(n, vector<vector<long long>>(n, vector<long long>(2, 0)));
+
+    // Base cases: single character
+    for (int i = 0; i < n; i++) {
+        if (exp[i] == 'T') {
+            dp[i][i][1] = 1;
+            dp[i][i][0] = 0;
+        } else if (exp[i] == 'F') {
+            dp[i][i][0] = 1;
+            dp[i][i][1] = 0;
         }
-        for(int i = n-1;i>=0;i--){
-            for(int j = 0;j<n;j++){
-                 long long ways = 0;
-                 for(int ind =i+1;ind<j-1;j+=2){
-                    long long lT = dp[i][ind-1][1];
-                    long long lF = dp[i][ind-1][0];
-                    long long rT = dp[ind+1][j][1];
-                    long long rF = dp[ind+1][j][0];
-                    if(exp[ind] == '&'){
-                        if(1) ways = (ways + (rT*lT)%mod)%mod;
-                       else{
-                         ways = (ways +(rT*lF)%mod +(lT*rF)%mod + (rF*lF)%mod)%mod;
-                        }
-                    }  
-                    else if(exp[ind] == '|'){
-                    if(1){
-                        ways = (ways + (lT*rT)%mod +(lT*rF)%mod + (lF*rT)%mod)%mod;
-                    }
-                    else{
-                    ways = (ways + (rF*lF)%mod)%mod;
-                    }
-                }
-                else{
-                if(1){
-                    ways = (ways +(rT*lF)%mod + (rF*lT)%mod)%mod;
-                }
-                else{
-                    // T^T =F , T^F = T , F^T = T,F^F = F
-                    ways = (ways + (rT*lT)%mod + (rF*lF)%mod)%mod;
-                }
-            }
-        }
-            dp[i][j][1] = ways;
-            }
-        }
-        return dp[0][n-1][1];
     }
+
+    // Length of substring
+    for (int len = 3; len <= n; len += 2) { // must include at least "T op T"
+        for (int i = 0; i + len - 1 < n; i++) {
+            int j = i + len - 1;
+            dp[i][j][0] = dp[i][j][1] = 0; // initialize
+
+            for (int ind = i + 1; ind <= j - 1; ind += 2) {
+                long long lT = dp[i][ind - 1][1];
+                long long lF = dp[i][ind - 1][0];
+                long long rT = dp[ind + 1][j][1];
+                long long rF = dp[ind + 1][j][0];
+
+                if (exp[ind] == '&') {
+                    dp[i][j][1] = (dp[i][j][1] + (lT * rT) % mod) % mod;
+                    dp[i][j][0] = (dp[i][j][0] + (lF * rT) % mod + (lT * rF) % mod + (lF * rF) % mod) % mod;
+                }
+                else if (exp[ind] == '|') {
+                    dp[i][j][1] = (dp[i][j][1] + (lT * rT) % mod + (lT * rF) % mod + (lF * rT) % mod) % mod;
+                    dp[i][j][0] = (dp[i][j][0] + (lF * rF) % mod) % mod;
+                }
+                else if (exp[ind] == '^') {
+                    dp[i][j][1] = (dp[i][j][1] + (lT * rF) % mod + (lF * rT) % mod) % mod;
+                    dp[i][j][0] = (dp[i][j][0] + (lT * rT) % mod + (lF * rF) % mod) % mod;
+                }
+            }
+        }
+    }
+
+    return dp[0][n - 1][1]; // number of ways to make whole exp = True
+}
 };
